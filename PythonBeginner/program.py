6aa328cb5bb34c9b36c38c983b640eeff8668613
Joe Parr <joe.parr@hotmail.co.uk>
2022-11-06 16:14:15 +0000

    .

---
+++ PythonBeginner/program.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+
+sum = 6 + 20
+print(sum , "is the number of 6 plus 20")
+
+product = sum * 3
+print(product , "is the number of sum times 3")
+
+print("the date today is", date.today())
